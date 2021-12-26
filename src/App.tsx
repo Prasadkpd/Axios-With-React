855c9ed598efe8f74731febe727e2e5108737cb8
Prasadkpd <2019cs078@stu.ucsc.lk>
2021-12-26 21:39:53 +0530

    make post request done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios, {AxiosResponse} from "axios";
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 interface post {
   title: string,
@@ -12,17 +12,29 @@ export default function App() {
   const [post, setPost] = useState<post | null>(null);
 
   useEffect(() => {
-    axios.get(baseURL).then((response: AxiosResponse) => {
+    axios.get(`${baseURL}/1`).then((response: AxiosResponse) => {
       setPost(response.data);
     });
   }, []);
 
-  if (!post) return null;
+  function createPost() {
+    axios
+        .post(baseURL, {
+          title: "Hello World",
+          body: "This is a new post"
+        })
+        .then((response)=> {
+          setPost(response.data);
+        })
+  }
+
+  if (!post) return <h1>No Post</h1>;
 
   return (
       <div>
         <h1>{post.title}</h1>
         <p>{post.body}</p>
+        <button onClick={createPost}>Create Post</button>
       </div>
   );
 }
